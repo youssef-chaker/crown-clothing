7f5508b01622e8292e4f2db296309180667728d7
youssef <kikoulolz1youssef@gmail.com>
2020-08-14 19:25:14 +0100

    first uno one wehed un

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./homepage.component";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
