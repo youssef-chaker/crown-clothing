a5c4a71652b8a660e08c96595195e4dc04ba8752
youssef <kikoulolz1youssef@gmail.com>
2020-08-15 20:35:20 +0100

    third

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path="/" component={Homepage}></Route>
+        {/* <Route path="/hats" component={HatsPage}></Route> */}
+      </Switch>
     </div>
   );
 }
